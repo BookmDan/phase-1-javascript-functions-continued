41df638ded7045c1e37ce85b4c4871c5e12dfeae
BookmDan <danoh329@gmail.com>
2023-07-18 11:22:13 -0700

    pass

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(param = "roller-skate") {
+  return `This Saturday, I want to ${param}!`;
+}
+
+function mondayWork(param = 'go to the office') {
+  return `This Monday, I will ${param}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function inner(def= "special") {
+    return `You are ${flair}${def}${flair}!`
+  }
+}
